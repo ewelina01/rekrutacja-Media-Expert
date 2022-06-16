d32d34ed7b6aab712754171a7c374b5d040fdd56
nnn889 <ewelina.ozga.78@gmail.com>
2022-06-17 00:50:00 +0200

    update

---
+++ build/temp/development/MyApp/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\xampp\htdocs\cwiczenia\rekrutacjaMediaExpert\MyApp/build/.sass-cache'
---
+++ build/temp/development/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\xampp\htdocs\cwiczenia\rekrutacjaMediaExpert\MyApp\app.js
